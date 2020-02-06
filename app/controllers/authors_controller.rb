8a08e4604b3ed183b0deb965c18a57c41af94c5b
AcnologiatheEnd <18.chung.winston@ctkprep.org>
2020-02-06 09:12:56 -0500

    Done

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,6 +1,6 @@
 class AuthorsController < ApplicationController
+  before_action :set_post!, only: [:show, :edit, :update]
   def show
-    @author = Author.find(params[:id])
   end
 
   def new
@@ -8,14 +8,34 @@ class AuthorsController < ApplicationController
   end
 
   def create
-    @author = Author.create(author_params)
-
-    redirect_to author_path(@author)
+    @author = Author.new(author_params)
+    
+    if @author.valid?
+      @author.save
+      redirect_to author_path(@author)
+    else 
+      render :new 
+    end
+  end
+  
+  def edit 
+  end
+  
+  def update
+    if @author.update(author_params)
+      redirect_to author_path(@author)
+    else
+      render :edit
+    end
   end
 
   private
 
   def author_params
     params.permit(:name, :email, :phone_number)
+  end
+
+  def set_post!
+    @author = Author.find(params[:id])
   end
 end
